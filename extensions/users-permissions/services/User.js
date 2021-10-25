e03df8f5a17707040acd5ef71ff0a4747e200005
Adi <nuta.adi@gmail.com>
2021-10-25 03:00:34 +0200

    auth + onboarding apis

---
+++ extensions/users-permissions/services/User.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { env } = require("strapi-utils");
+const smsClient = require("twilio")(env("TWILIO_ID"), env("TWILIO_TOKEN"));
+
+module.exports = {
+  async sendEmailToken(user) {
+    const emailToken = Math.floor(Math.random() * 90000) + 10000;
+
+    await this.edit({ id: user.id }, { emailToken });
+
+    await strapi.plugins["email-designer"].services.email.sendTemplatedEmail(
+      {
+        to: user.email,
+      },
+      {
+        templateId: 1,
+        sourceCodeToTemplateId: 1,
+        subject: `${emailToken} is your passcode for CoFoundersLab`,
+      },
+      {
+        token: emailToken,
+      }
+    );
+  },
+
+  async sendSmsToken(user) {
+    const phoneToken = Math.floor(Math.random() * 90000) + 10000;
+
+    await this.edit({ id: user.id }, { phoneToken });
+
+    await smsClient.messages.create({
+      to: user.phone,
+      from: env("TWILIO_PHONE"),
+      body: `Your verification code is ${phoneToken}`,
+    });
+  },
+};

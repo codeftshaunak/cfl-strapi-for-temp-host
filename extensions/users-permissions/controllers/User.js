c044a353e34e9f5c47b885c50e9e12cd4360bf7e
Adi <nuta.adi@gmail.com>
2021-12-22 22:15:01 +0100

    update last login on /me

---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const _ = require("lodash");
+const { sanitizeEntity } = require("strapi-utils");
+
+const sanitizeUser = (user) =>
+  sanitizeEntity(user, {
+    model: strapi.query("user", "users-permissions").model,
+  });
+
+module.exports = {
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    await strapi.query("user", "users-permissions").update(
+      { id: user.id },
+      {
+        lastLogin: new Date(),
+      }
+    );
+
+    ctx.body = sanitizeUser(user);
+  },
+};

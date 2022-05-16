1c631013f2e2fb80dd3ee8fe5f2f18e4b9c10cde
Adi <nuta.adi@gmail.com>
2022-05-16 20:56:31 +0200

    fix: send email on new contact message

--- api/contact/controllers/contact.js
+++ api/contact/controllers/contact.js
@@ -1,8 +1,42 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.contact.create(data, { files });
+    } else {
+      entity = await strapi.services.contact.create(ctx.request.body);
+    }
+
+    await strapi.plugins["email-designer"].services.email.sendTemplatedEmail(
+      {
+        to: "adi@cofounderslab.com",
+        replyTo: ctx.request.body.email,
+      },
+      {
+        templateId: 5,
+        sourceCodeToTemplateId: 5,
+      },
+      {
+        ...ctx.request.body,
+      }
+    );
+
+    return sanitizeEntity(entity, { model: strapi.models.contact });
+  },
+};

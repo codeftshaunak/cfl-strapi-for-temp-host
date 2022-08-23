1b513915a6006cfacbeb24a809184f749c21c864
Lovey <lovepreet@cofounderslab.com>
2022-08-23 22:35:18 +0530

    changes for undock access token and profile url

--- api/advisor-profile/services/advisor-profile.js
+++ api/advisor-profile/services/advisor-profile.js
@@ -5,4 +5,36 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+    buildSearchParams(user, query) {
+        let params = {
+          public: true,
+        //   user: { $exists : true, $not : null },
+        };
+        if (user) {
+        //   params["user"] = { $ne: '' };
+        }
+        if (query.country) {
+          params["countryCode"] = query.country;
+        }
+        if (query.city) {
+          params["city"] = query.city;
+        }
+        if (query.title) {
+            params["title"] = query.title;
+          }
+        // if (query.skills && query.skills.length > 0) {
+        //   params["skills"] = { $in: query.skills };
+        // }
+        if (query.q) {
+          params = {
+            ...params,
+            ...{
+              $text: { $search: query.q },
+            },
+          };
+        }
+    
+        return params;
+      },
+};

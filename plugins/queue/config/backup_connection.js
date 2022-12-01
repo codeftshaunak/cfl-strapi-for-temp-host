33c06e274a978628a584a9fcf7360d3a373772e6
Lovey <lovepreet@cofounderslab.com>
2022-12-01 19:45:17 +0530

    changes to redis connection page

--- plugins/queue/config/backup_connection.js
+++ plugins/queue/config/backup_connection.js
@@ -13,23 +13,29 @@ const opts = {
       case "client":
         if (!client) {
           client = new Redis(env("REDIS_URL"), {
-            maxRetriesPerRequest: null,
-            enableReadyCheck: false
+            ...redisOpts,
+            tls: {
+              rejectUnauthorized: false,
+            },
           });
         }
         return client;
       case "subscriber":
         if (!subscriber) {
           subscriber = new Redis(env("REDIS_URL"), {
-            maxRetriesPerRequest: null,
-            enableReadyCheck: false
+            ...redisOpts,
+            tls: {
+              rejectUnauthorized: false,
+            },
           });
         }
         return subscriber;
       case "bclient":
         return new Redis(env("REDIS_URL"), {
-          maxRetriesPerRequest: null,
-          enableReadyCheck: false
+          ...redisOpts,
+          tls: {
+            rejectUnauthorized: false,
+          },
         });
       default:
         throw new Error("Unexpected connection type: ", type);

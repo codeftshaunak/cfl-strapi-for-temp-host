185c40281fab83d4a235da765e7d78eb9c3372e7
Lovey <lovepreet@cofounderslab.com>
2023-02-10 03:34:55 +0530

    fix: changes for trending posts titles

--- api/discussion-topic/controllers/discussion-topic.js
+++ api/discussion-topic/controllers/discussion-topic.js
@@ -1,8 +1,21 @@
 'use strict';
 
+const { sanitizeEntity } = require("strapi-utils/lib");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async findTrending(ctx) {
+        let entities;
+        const params = ctx.query;
+        entities = await strapi.services["discussion-topic"].find(params);
+
+        return entities.map((entity) => {
+          entity.discussions = entity.discussions.length;
+          return sanitizeEntity(entity, { model: strapi.models["discussion-topic"]  });
+        });
+      },
+};

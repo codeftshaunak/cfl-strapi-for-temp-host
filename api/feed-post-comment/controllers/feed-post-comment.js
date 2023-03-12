b1784036c8a2b73a586ac1a260e062269a269c72
Lovey <lovepreet@cofounderslab.com>
2023-03-13 01:38:58 +0530

    changes for comment reply

--- api/feed-post-comment/controllers/feed-post-comment.js
+++ api/feed-post-comment/controllers/feed-post-comment.js
@@ -8,7 +8,6 @@ const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   async findCommentsByPostId(ctx) {
-    console.log(ctx.params);
     const { id } = ctx.params;
     const entities = await strapi
       .query("feed-post-comment")
@@ -22,8 +21,60 @@ module.exports = {
         },
       })
       .sort([["createdAt", "desc"]]);
+      return entities;
+  },
 
-    return entities;
+  async getReplies(ctx){
+    const ids = JSON.parse(ctx.request.body?.ids);
+    //const replies = await strapi.services["feed-post-comment"].find({id: {$in:ids}});
+    let params = {};
+    params["_id"] = { $in: ids };
+    const replies = await strapi
+      .query("feed-post-comment")
+      .model.find(params)
+      .populate({
+        path: "user",
+        populate: {
+          path: "profile",
+        },
+      })
+      .sort([["createdAt", "asc"]]);
+    return replies;
+  },
+
+  async reply(ctx) {
+    const {id} = ctx.params;
+    const {commentId} = ctx.request.body;
+    const reply = await strapi.services["feed-post-comment"].create(ctx?.request?.body);
+    const comment = await strapi.services["feed-post-comment"].findOne({id:commentId});
+    let replies = comment.replies || [];
+    replies.push(reply.id);
+    // console.log(comment);
+    const toProfile = await strapi.services["profile"].getProfile(comment.user?.profile);
+
+    // send email only if the user is not the comment author
+    if(comment.user.id!==ctx.state.user.id){
+      try{
+        await strapi.plugins["email-designer"].services["email"].sendTemplatedEmail(
+          {to:comment.user.email},
+          {
+            templateId: 10,
+            sourceCodeToTemplateId: 10,
+          },
+          {
+            toProfile,
+            fromProfile: ctx.state.user.profile,
+          }
+        );
+      }catch(e){
+        console.log("error while sending message email ",e.message);
+      }
+    }
+
+    //adding replies ids to the comment data
+    await strapi.services["feed-post-comment"].update({id: commentId},{replies});
+
+    return sanitizeEntity(reply, {model: strapi.models["feed-post-comment"]});
   },
 
   async create(ctx) {
@@ -40,10 +91,7 @@ module.exports = {
     const { user: userReceiver } = post;
 
     let commentAuthor = ctx.state.user;
-    let postAuthor = await strapi.services["profile"].findById(post.user.profile);
-    delete postAuthor.connections;
-    delete postAuthor.discussions;
-    delete postAuthor.discussion_replies;
+    let postAuthor = await strapi.services["profile"].getProfile(post.user.profile);
 
     if(!post.user.blocked){
       try{

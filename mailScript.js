80e71feea28484ec3e00bc8fd95b2f575edee876
Lovey <lovepreet@cofounderslab.com>
2023-04-05 01:12:06 +0530

    changes to check error

--- mailScript.js
+++ mailScript.js
@@ -2,5 +2,7 @@ var request = require('request');
 request('https://api-cfl.herokuapp.com/autoemails/shoot', function (error, response, body) {
     if (!error && response.statusCode === 200) {
         console.log("Email Script started...") // Print the google web page.
+     }else{
+        console.log(error);
      }
 })
